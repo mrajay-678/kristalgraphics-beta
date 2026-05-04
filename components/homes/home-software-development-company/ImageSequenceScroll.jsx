965b4e78dbb9bc587de0e089b1de1cfed9a77c00
Ajay Tiwari <121260828+mrajay-678@users.noreply.github.com>
2026-05-04 13:51:57 +0530

    Adjust canvas width to 90vw

--- components/homes/home-software-development-company/ImageSequenceScroll.jsx
+++ components/homes/home-software-development-company/ImageSequenceScroll.jsx
@@ -58,7 +58,7 @@ const ImageSequenceScroll = () => {
 
   return (
     <div style={{ minHeight: "100vh" }}>
-      <canvas className="mx-20 !w-12/12" ref={canvasRef} />
+      <canvas className="mx-20 !w-[90vw]" ref={canvasRef} />
     </div>
   );
 };
