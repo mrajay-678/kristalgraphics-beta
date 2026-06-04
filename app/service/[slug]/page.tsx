d10b865784252e2f14ab269315db209743602201
Ajay Tiwari <121260828+mrajay-678@users.noreply.github.com>
2026-06-05 02:07:26 +0530

    Service page

--- app/service/[slug]/page.tsx
+++ app/service/[slug]/page.tsx
@@ -19,7 +19,7 @@ export default async function ServicePage({
   return (
     <main className="pt-60">
       <div className="w-11/12 mx-auto">
-        <h1 className="text-7xl font-heading">
+        <h1 className="text-7xl font-heading leading-normal">
           {service.question}
         </h1>
 
