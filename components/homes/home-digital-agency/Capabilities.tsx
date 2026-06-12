0170a6f749e02d6c7030af49f16cd96d04529745
Ajay Tiwari <121260828+mrajay-678@users.noreply.github.com>
2026-06-13 00:39:07 +0530

    Capabilities

--- components/homes/home-digital-agency/Capabilities.tsx
+++ components/homes/home-digital-agency/Capabilities.tsx
@@ -74,67 +74,7 @@ export default function Capabilities() {
         </div>
         {/* Block - Section Title End */}
         {/* Block - Our Capabilities List Start */}
-        <div className="mxd-block">
-          <div className="container-fluid p-0">
-            <div className="row g-0">
-              <div className="col-12 mxd-grid-item no-margin">
-                <div className="mxd-cpb-list">
-                  {capabilities.map((item: compatibility, idx: number) => (
-                    <div
-                      className="mxd-cpb-list__item hover-reveal__item"
-                      key={idx}
-                      onMouseMove={(e) => handleMouseMove(e, idx)}
-                      onMouseLeave={handleMouseLeave}
-                    >
-                      <div className="mxd-cpb-list__divider anim-uni-in-up" />
-                      <div
-                        style={{
-                          opacity: hoverState.activeIndex === idx ? 1 : 0,
-                          transform: "translate(-80%, -50%)",
-                          left: hoverState.x,
-
-                          pointerEvents: "none",
-                          transition: "opacity 0.3s ease",
-                        }}
-                        className="hover-reveal__content hover-reveal-280x340"
-                      >
-                        <Image
-                          className="hover-reveal__image"
-                          alt="Project Preview"
-                          src={item.hoverImage}
-                          width={600}
-                          height={730}
-                          style={{
-                            transform:
-                              hoverState.activeIndex === idx
-                                ? "scale(1,1)"
-                                : "scale(1,1.4)",
-                            transition: "transform 0.3s ease",
-                          }}
-                        />
-                      </div>
-                      <div className="mxd-cpb-list__content anim-uni-in-up">
-                        <h6 className="mxd-cpb-list__title">{item.title}</h6>
-                        <div className="mxd-cpb-list__num">
-                          <span>{item.num}</span>
-                        </div>
-                      </div>
-                      <div className="mxd-cpb-list__image anim-uni-in-up">
-                        <Image
-                          alt="Project Preview"
-                          src={item.mainImage}
-                          width={1200}
-                          height={800}
-                        />
-                      </div>
-                      <div className="mxd-cpb-list__divider anim-uni-in-up" />
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+       
         {/* Block - Our Capabilities List Start */}
       </div>
     </div>
